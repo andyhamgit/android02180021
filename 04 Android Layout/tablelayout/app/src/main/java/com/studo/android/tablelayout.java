45811d256200e42b276846ebbef76355917b0d09
AndyHam <andyham.g@gmail.com>
2017-03-14 23:28:56 +0800

    add lab4

---
+++ 04 Android Layout/tablelayout/app/src/main/java/com/studo/android/tablelayout.java
@@ -0,0 +1,15 @@
+
+package com.studo.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class tablelayout extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
